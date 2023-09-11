34d225803adc37b1c1d273ef2c605c0dd256266c
chihiro inoue <chihiro-inoue@g.ecc.u-tokyo.ac.jp>
2023-09-11 19:30:22 +0900

    rewrite data cleaning code

--- code/cleaning.R
+++ code/cleaning.R
@@ -1,18 +1,22 @@
 # read distributed data --------------------------------------------------------
 # student characteristics
-df_student_characteristics <- read_csv(file = paste0(dir_distributed_data, "/student_characteristics.csv"),
-                                       skip = 0,  # 指定した列をスキップする（この場合はなくても変わらない）
-                                       locale = locale(encoding = "shift-jis")) 
+df_student_characteristics <- 
+  read_csv(file = paste0(dir_distributed_data, "/student_characteristics.csv"),
+           skip = 0,  # 指定した列をスキップする（この場合はなくても変わらない）
+           locale = locale(encoding = "shift-jis")) 
 
 # treatment status
-df_treatment_status <- read_csv(file = paste0(dir_distributed_data, "/school_characteristics.csv"),
-                                skip = 0,
-                                locale = locale(encoding = "UTF-8"))
+df_treatment_status <- 
+  read_csv(file = paste0(dir_distributed_data, "/school_characteristics.csv"),
+           skip = 0,
+           locale = locale(encoding = "UTF-8"))
 
 # outcomes
-df_outcomes <- read_csv(file = paste0(dir_distributed_data, "/outcomes.csv"),
-                        skip = 0,
-                        locale = locale(encoding = "shift-jis"))
+df_outcomes <- 
+  read_csv(file = paste0(dir_distributed_data, "/outcomes.csv"),
+           col_types = "cd",
+           skip = 0,
+           locale = locale(encoding = "shift-jis"))
 
 
 
@@ -45,11 +49,10 @@ df_outcomes <- df_outcomes %>%
 
 
 # clean data -------------------------------------------------------------------
-# garbles
 
+# garbles
 df_student_characteristics <- df_student_characteristics %>% 
-  mutate(student_id = str_remove(student_id, "番"),  # mutateで複数列を作成する場合はカンマで繋げられる
-         student_id = as.numeric(student_id))
+  mutate(student_id = str_remove(student_id, "番"))  # mutateで複数列を作成する場合はカンマで繋げられる
 
 # missing values
 missing_pattern <- c("unknown", NA, "999", "*", "###")
@@ -60,32 +63,33 @@ df_student_characteristics <-
                 ~if_else(.x %in% missing_pattern, NA_character_, .x))) %>% 
   mutate(percentile = as.numeric(percentile))
 
+
+# join data ---------------------------------------------------------------
+
+df_concatenated <- df_student_characteristics %>% 
+  left_join(df_treatment_status, by = "school_id") %>%  
+  left_join(df_outcomes, by = "student_id")
+
+
+
+# make variables ----------------------------------------------------------
+
 # make dummy variables
-df_numeric <- df_cleaned %>% 
+df <- df_concatenated %>% 
   mutate(is_girl = if_else(gender == "女子", 1, 0),
-         in_bungoma = if_else(district == "BUNGOMA", 1, 0),
-         in_butere = if_else(district == "BUTERE/M", 1, 0),
-         bottom_half = if_else(half == "下位50%", 1, 0),
-         top_half = if_else(half == "上位50%", 1, 0),
-         bottom_quarter = if_else(quartile == "下位25%", 1, 0),
-         second_quarter = if_else(quartile == "下位25-50%", 1,0),
-         third_quarter = if_else(quartile == "上位25-50%", 1, 0),
-         top_quarter = if_else(quartile == "上位25%", 1, 0))
+         in_bungoma = if_else(district == "BUNGOMA", 1, 0)) %>% 
+  fastDummies::dummy_cols(select_columns = c("half", "quarter"), ignore_na = T) %>% 
+  rename_with(.cols = paste("half", c("上位50%", "下位50%"), sep = "_"), 
+              .fn = ~{paste("half", c("top", "bottom"), sep = "_")}) %>% 
+  rename_with(.cols = paste("quarter", c("上位25%", "上位25-50%", "下位25-50%", "下位25%"), sep = "_"), 
+              .fn = ~{paste("quarter", c("top", "second", "third", "bottom"), sep = "_")}) %>% 
+  select(-gender, -half, -quarter)
 
 
 # scale score
-df <- df_numeric %>% 
-  mutate(std_score = drop(scale(score)))  # scale関数の返り値はmatrixなのでdrop関数でベクトルに直す
-
-
-# concatenate all data ---------------------------------------------------------
-
-
-df_concatenated <- df_student_characteristics %>% 
-  left_join(df_treatment_status, by = "sch_id") %>%  # キー列を複数指定するときはby = c("a", "b")
-  left_join(df_outcomes, by = "stu_id")
+df <- df %>% 
+  mutate(std_score = drop(scale(score)))  
 
 
 # output cleaned data ----------------------------------------------------------
-saveRDS(df_treatment_status, paste0(dir_cleaned_data, "/df_school.rds"))
-saveRDS(df, paste0(dir_cleaned_data, "/df.rds"))
+saveRDS(df, paste0(dir_cleaned_data, "/df_clean.rds"))
